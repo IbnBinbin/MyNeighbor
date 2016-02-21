4d3533837cc06ac3a5bbab06c70a413edef2c72e
ttnok <ttnok27@gmail.com>
2016-02-21 12:06:55 +0000

    added local database template

---
+++ app/src/main/java/ibn/myneighbor/LocalStorageAdapter.java
@@ -0,0 +1,251 @@
+package ibn.myneighbor;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by ttnok on 21/2/2559.
+ */
+
+public class LocalStorageAdapter extends SQLiteOpenHelper {
+
+    // Logcat tag
+    private static final String LOG = "Ibn";//LocalStorageAdapter.class.getName();
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "my_neighbor";
+
+    // Table Names
+    private static final String TABLE_CONVERSATION = "conversation";
+    private static final String TABLE_USER = "user";
+    private static final String TABLE_ACTIVITY = "sctivity";
+    private static final String TABLE_GROUP = "group";
+    private static final String TABLE_NEIGHBORHOOD = "neighborhood";
+
+    // Column names
+    private static final String KEY_ID = "id";
+    private static final String KEY_TOPIC = "topic";
+    private static final String KEY_OWNER = "owner";
+    private static final String KEY_CLIENT = "client";
+    private static final String KEY_CREATED_AT = "created_at";
+    private static final String KEY_CHAT_MESSAGE = "chat_message";
+
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_BIO = "bio";
+    private static final String KEY_PROFILE_PIC_ID = "profile_pic_id";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_UPDATED_AT = "updated_at";
+
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_REQUEST_OR_OFFER = "request_or_offer";
+    private static final String KEY_GROUP_NAME = "group_name";
+    private static final String KEY_EXPIRT_DATE = "expire_date";
+
+    private static final String KEY_MEMBER = "member";
+
+    private static final String KEY_INITIAL_POINT = "initial_point";
+    private static final String KEY_FINAL_POINT = "final_point";
+    private static final String KEY_DRAW_TYPE = "draw_type";
+
+    // Table Create Statements
+    // Conversation table create statement
+    private static final String CREATE_TABLE_CONVERSATION = "CREATE TABLE " + TABLE_CONVERSATION
+            + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_TOPIC + " TEXT," + KEY_OWNER + " TEXT,"
+            + KEY_CLIENT + " TEXT," + KEY_CREATED_AT + " DATETIME," + KEY_CHAT_MESSAGE + " TEXT" +  ")";
+    // User table create statement
+    private static final String CREATE_TABLE_USER = "CREATE TABLE "
+            + TABLE_USER + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"+ KEY_USERNAME + " TEXT PRIMARY KEY," + KEY_BIO
+            + " TEXT," + KEY_PROFILE_PIC_ID + " INTEGER," + KEY_PASSWORD + " TEXT," + KEY_CREATED_AT + " DATETIME,"
+            + KEY_UPDATED_AT + " DATETIME" + ")";
+
+    // Activity table create statement
+    private static final String CREATE_TABLE_ACTIVITY = "CREATE TABLE " + TABLE_ACTIVITY
+            + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_TITLE + " TEXT,"+ KEY_DESCRIPTION + " TEXT,"
+            + KEY_REQUEST_OR_OFFER + " INTEGER," + KEY_GROUP_NAME+ " INTEGER," + KEY_EXPIRT_DATE + " DATETIME,"
+            + KEY_CREATED_AT + " DATETIME," + KEY_UPDATED_AT + " DATETIME," + KEY_OWNER + " TEXT" + ")";
+
+    // Group table create statement
+    private static final String CREATE_TABLE_GROUP = "CREATE TABLE " + TABLE_GROUP
+            + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_OWNER + " TEXT,"
+            + KEY_GROUP_NAME + " TEXT," + KEY_MEMBER + " TEXT" +  ")";
+
+    // Activity table create statement
+    private static final String CREATE_TABLE_NEIGHBORHOOR = "CREATE TABLE " + TABLE_NEIGHBORHOOD
+            + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_INITIAL_POINT + " TEXT,"+ KEY_FINAL_POINT+ " TEXT,"
+            + KEY_DRAW_TYPE + " INTEGER," + KEY_OWNER+ " TEXT"  + ")";
+
+
+    public LocalStorageAdapter(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // creating required tables
+        db.execSQL(CREATE_TABLE_CONVERSATION);
+        db.execSQL(CREATE_TABLE_USER);
+        db.execSQL(CREATE_TABLE_ACTIVITY);
+        db.execSQL(CREATE_TABLE_GROUP);
+        db.execSQL(CREATE_TABLE_NEIGHBORHOOR);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONVERSATION);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ACTIVITY);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_GROUP);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NEIGHBORHOOD);
+        // create new tables
+        onCreate(db);
+    }
+
+    public void deleteAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_CONVERSATION, null, null);
+        db.delete(TABLE_USER, null, null);
+        db.delete(TABLE_ACTIVITY, null, null);
+        db.delete(TABLE_GROUP, null, null);
+        db.delete(TABLE_NEIGHBORHOOD, null, null);
+    }
+
+    public long createUser(String userName, String bio, int profilePic, String password) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_USERNAME, userName);
+        values.put(KEY_BIO, bio);
+        values.put(KEY_PROFILE_PIC_ID, profilePic);
+        values.put(KEY_PASSWORD, password);
+        values.put(KEY_CREATED_AT, getDateTime());
+        values.put(KEY_UPDATED_AT, getDateTime());
+        long check = db.insert(TABLE_USER, null, values);
+
+        return check;
+    }
+
+    public long createActivity(String title, String desc, int req_offer, String groupName, Date expire, String owner) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_TITLE, title);
+        values.put(KEY_DESCRIPTION, desc);
+        values.put(KEY_REQUEST_OR_OFFER, req_offer);
+        values.put(KEY_GROUP_NAME, groupName);
+        values.put(KEY_EXPIRT_DATE, getDateTime(expire));
+        values.put(KEY_CREATED_AT, getDateTime());
+        values.put(KEY_UPDATED_AT, getDateTime());
+        values.put(KEY_OWNER, owner);
+        long check = db.insert(TABLE_ACTIVITY, null, values);
+
+        return check;
+    }
+
+    public long createGroup(String owner, String groupName, String member) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_OWNER, owner);
+        values.put(KEY_GROUP_NAME, groupName);
+        values.put(KEY_MEMBER, member);
+        long check = db.insert(TABLE_GROUP, null, values);
+
+        return check;
+    }
+
+    public long createNeighborhood(String init, String finalP, int drawType, String owner) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_INITIAL_POINT, init);
+        values.put(KEY_FINAL_POINT, finalP);
+        values.put(KEY_DRAW_TYPE, drawType);
+        values.put(KEY_OWNER, owner);
+        long check = db.insert(TABLE_NEIGHBORHOOD, null, values);
+
+        return check;
+    }
+
+    public int updateActivity(int id, String title, String desc, int req_offer, String groupName, Date expire, String owner){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_TITLE, title);
+        values.put(KEY_DESCRIPTION, desc);
+        values.put(KEY_REQUEST_OR_OFFER, req_offer);
+        values.put(KEY_GROUP_NAME, groupName);
+        values.put(KEY_EXPIRT_DATE, getDateTime(expire));
+        values.put(KEY_UPDATED_AT, getDateTime());
+        values.put(KEY_OWNER, owner);
+//        Log.d("Nok", "update task: "+values.valueSet());
+        return db.update(TABLE_ACTIVITY, values, KEY_ID + "=?", new String[] {String.valueOf(id)});
+    }
+
+    public int updateUser(int id, String userName, String bio, int profilePic, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_USERNAME, userName);
+        values.put(KEY_BIO, bio);
+        values.put(KEY_PROFILE_PIC_ID, profilePic);
+        values.put(KEY_PASSWORD, password);
+        values.put(KEY_UPDATED_AT, getDateTime());
+//        Log.d("Nok", "update task: "+values.valueSet());
+        return db.update(TABLE_USER, values, KEY_ID + "=?", new String[]{String.valueOf(id)});
+    }
+
+//    public List<Integer> getAllAlarmIDs(int occurrence_id) {
+//        ArrayList<Integer> alarmIDs = new ArrayList<Integer>();
+//        String selectQuery = "SELECT  * FROM " + TABLE_ALARMIDS + " WHERE "
+//                + KEY_OCCURRENCE_ID + " = " + occurrence_id;
+//        String selectQuery = "SELECT  * FROM " + TABLE_OCCURRENCES +" WHERE "+KEY_REMINDER_ID +"="+reminder_id;
+//        String selectQuery = "SELECT  * FROM " + TABLE_TASKS +" WHERE "+KEY_REMINDER_ID +"="+reminder_id;
+//
+//
+//        SQLiteDatabase db = this.getReadableDatabase();
+//        Cursor c = db.rawQuery(selectQuery, null);
+//
+//        if (c.moveToFirst()) {
+//            do {
+//                alarmIDs.add(c.getInt(c.getColumnIndex(KEY_VALUE_ALAREMID)));
+//            } while (c.moveToNext());
+//        }
+//
+//        return alarmIDs;
+//    }
+
+    // closing database
+    public void closeDB() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        if (db != null && db.isOpen())
+            db.close();
+    }
+
+    /**
+     * get datetime
+     * */
+    private String getDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+    private String getDateTime(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        return dateFormat.format(date);
+    }
+}
---
+++ app/src/main/java/ibn/myneighbor/ParseStorageAdapter.java
@@ -0,0 +1,65 @@
+package ibn.myneighbor;
+
+import android.content.Context;
+import android.util.Log;
+
+//import com.parse.*;
+
+/**
+ * Created by ttnok on 20/10/2558.
+ */
+public class ParseStorageAdapter {
+
+//    public ParseStorageAdapter(Context context){
+//        // Enable Local Datastore.
+//        Parse.enableLocalDatastore(context);
+//        Parse.initialize(context, "cUMU855gEthkMhDtCaRzcPNoY88qs0Q4Cegve4LM", "zgxQ24kOnuFONN6zTTe0b3T7fuKXtkjxIU784lKh");
+//
+//    }
+//    public void testAddNewTaskToDB(){
+//        //test
+//        ParseObject testObject = new ParseObject("TestObject");
+//        testObject.put("Blabla", "bagannnnnnnnn");
+//        testObject.saveEventually();
+////        testObject.saveInBackground();
+//    }
+//
+//    public void createLogAlert (int user_id, Task task){
+//        ParseObject logAlert = new ParseObject("LogAlert");
+//        logAlert.put("USER_ID", user_id);
+//        logAlert.put("TASK_NAME", task.getName());
+//        logAlert.put("TIMESTAMP", LocalDateTime.now().toString());
+////        logAlert.saveInBackground();
+//        logAlert.saveEventually(new SaveCallback() {
+//            public void done(ParseException e) {
+//                if (e == null) {
+//                    // Saved successfully.
+//                    Log.d("Nok", "finish log alert");
+//                } else {
+//                    // The save failed.
+//                    Log.d("Nok", "createLogAlert: " + e.getMessage());
+//                }
+//            }
+//        });;
+//    }
+//
+//    public void createLogTaskComplete (int user_id, Task task){
+//        ParseObject logTaskComplete = new ParseObject("LogCompleteTask");
+//        logTaskComplete.put("USER_ID", user_id);
+//        logTaskComplete.put("TASK_NAME", task.getName());
+//        logTaskComplete.put("TIMESTAMP", LocalDateTime.now().toString());
+////        logTaskComplete.saveInBackground();
+//        logTaskComplete.saveEventually(new SaveCallback() {
+//            public void done(ParseException e) {
+//                if (e == null) {
+//                    // Saved successfully.
+//                    Log.d("Nok", "finish log task complete");
+//                } else {
+//                    // The save failed.
+//                    Log.d("Nok", "createLogTaskComplete: "+ e.getMessage());
+//                }
+//            }
+//        });;
+//    }
+
+}
